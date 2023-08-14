21173cb8dc728aa35885a97675339572c2f81c3d
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-08-14 12:18:34 +0000

    20230814

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 _under construction_
 
 <h1>Apprication Name _NaganoCake_</h1>
-
-
-* URL:
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registations: "public/registrarions",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrarions, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+
 end
