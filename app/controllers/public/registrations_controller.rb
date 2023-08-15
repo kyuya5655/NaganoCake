0d06bfbf170c21bebfe28fd5fb669a4018e09396
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-08-15 22:11:37 +0000

    add_customers_strong_parameter

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def customers_params
-    params.require(:customers).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :firast_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
   end
+  
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
