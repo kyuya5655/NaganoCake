ea287d8d6d8470fd575b27b264ae67f568920f2f
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-08-22 13:34:47 +0000

    20230822

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,9 +6,14 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :firast_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
   end
-  
+
+  def after_sign_up_path_for(resource)
+    customers_path
+  end
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_for :customers, skip: [:passwords], controllers: {
-    registations: "public/registrarions",
+    registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  devise_for :admin, skip: [:registrarions, :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions',
   }
 
   namespace :admin do
     get '' => 'homes#top'
+  end
+
+  scope module: :public do
+    resource :customers, only: [:show, :edit, :update]
+    get '/' => 'homes#top'
+    get '/about' => 'homes#about'
+    get '/quit_check' => 'customers#quit_check'
+    patch '/withdraw' => 'customers#withdraw'
   end
 
 end
