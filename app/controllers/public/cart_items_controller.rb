fed1e0bf3b3c6d8877f4c5ebc72fd1f6801cab5d
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-09-06 15:16:31 +0000

    20230905

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,22 +1,32 @@
 class Public::CartItemsController < ApplicationController
 
+  # def create
+  #   # @item = Item.find(cart_item_params[:item_id])
+  #   @cart_item = CartItem.new(cart_item_params)
+  #   @cart_item.customer_id = current_customer.id
+  #   @cart_item.save
+  #   redirect_to cart_items_path
+  # end
+
   def create
-    @item = Item.find(cart_item_params[:item_id])
-    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = CartItem.find_by(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
-  end
-  
-  def create
-    CartItem.find_by(name params[])
+    @cart_items = CartItem.all
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+          new_amount = cart_item.amount += @cart_item.amount.to_i
+          cart_item.update(:cart_item)
+        # else
+        #   @cart_item.save
+
+        end
+      end
   end
 
   def index
     @cart_items = CartItem.all
     @sum = 0
   end
-
 
   def update
     @cart_item = CartItem.find(params[:id])
@@ -38,7 +48,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 
 end
